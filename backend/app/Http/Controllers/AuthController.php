55e852824134f7822219ce5379c448dc52091d90
Kiet Do Tuan <kietdtit@gmail.com>
2024-10-02 23:03:35 +0700

    Setup Language for Message

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -204,6 +204,7 @@ class AuthController extends Controller
 
     public function login()
     {
+        dd(__('messages.welcome'));
         $validator = Validator::make(request()->all(), [
             'email' => 'required|string|email|max:100',
             'password' => 'required|string|min:8',
---
+++ backend/resources/lang/vi/messages.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'welcome' => 'Chào mừng bạn đến với ứng dụng của chúng tôi!',
+    'user_signup_success' => 'Đăng ký thành công.',
+    'validation_error' => 'Có lỗi xác thực.',
+];
