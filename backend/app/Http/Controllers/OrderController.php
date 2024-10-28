f3761336d7cea67348878f71e9e4cdada835f53e
LangPN <langpn.dev@gmail.com>
2024-10-28 09:47:02 +0700

    Fix/Update Cart & Fix Order

--- backend/app/Http/Controllers/OrderController.php
+++ backend/app/Http/Controllers/OrderController.php
@@ -6,232 +6,126 @@ use App\Models\{Cart, CartItem, Order, OrderItem};
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\{Auth, DB, Log};
 use Illuminate\Support\Str;
-use Stripe\{PaymentIntent, PaymentMethod, Stripe, Customer};
+use Stripe;
 
 class OrderController extends Controller
 {
-    public function createOrder(Request $request)
+    public function store(Request $request)
     {
-        // Lấy thông tin người dùng hiện tại
+        // Get user & user's cart
         $user = Auth::user();
+        $cart = Cart::where('user_id', $user->id)->first();
+        $cartItems = CartItem::where('cart_id', $cart->id)->get();
+        if ($cartItems->isEmpty()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Cart is empty',
+            ], 400);
+        }
 
-        // Bắt đầu giao dịch
-        DB::beginTransaction();
+        // Calculate total price
+        $total = round($cartItems->reduce(function ($carry, $item) {
+            $price = $item->course->type_sale === 'percent'
+                ? $item->course->price - ($item->course->price * $item->course->sale_value / 100)
+                : $item->course->price - $item->course->sale_value;
+            return $carry + $price;
+        }, 0));
+        $currency = $request->currency ?? 'usd';
+        if ($currency == 'usd') {
+            $total = $total * 100;
+        }
+
+        // TODO Handle voucher later
+        $voucher = $request->voucher;
 
         try {
-            // Lấy giỏ hàng của người dùng
-            $cart = Cart::with('cartItems')->where('user_id', $user->id)->first();
-            if (!$cart || $cart->cartItems->isEmpty()) {
-                return response()->json(['status' => 'error', 'message' => 'Giỏ hàng trống'], 404);
-            }
+            DB::beginTransaction();
 
-            // Tính tổng giá trị từ giỏ hàng
-            $totalPrice = $cart->cartItems->sum('price');
-
-            // Tạo đơn hàng mới
-            $order = Order::create([
-                'user_id' => $user->id,
-                'voucher_id' => $request->voucher_id,
-                'order_code' => Str::random(10),
-                'total_price' => $totalPrice,
-                'payment_method' => $request->payment_method,
-                'payment_status' => 'pending',
-                'payment_code' => Str::uuid(),
-                'status' => 'active',
+            $stripe = new \Stripe\StripeClient(config('services.stripe.secret'));
+            $response = $stripe->checkout->sessions->create([
+                'line_items' => [
+                    [
+                        'price_data' => [
+                            'currency' => $currency,
+                            'product_data' => [
+                                'name' => $cartItems->map(function ($item) {
+                                    return $item->course->title;
+                                })->join(', '),
+                            ],
+                            'unit_amount' => $total,
+                        ],
+                        'quantity' => 1,
+                    ],
+                ],
+                'mode' => 'payment',
+                'success_url' => route('orders.success') . '?session_id={CHECKOUT_SESSION_ID}',
+                'cancel_url' => route('orders.cancel'),
             ]);
 
-            // Tạo các OrderItems từ CartItems
-            foreach ($cart->cartItems as $cartItem) {
+            dd($response);
+
+            $order = Order::create([
+                'user_id' => $user->id,
+                'voucher_id' => $voucher ?? null,
+                'order_code' => 'Edunity#' . Str::random(10),
+                'total_price' => $total,
+                'currency' => $currency,
+                'payment_method' => 'Stripe',
+                'payment_status' => 'pending',
+                'payment_code' => $response->id,
+            ]);
+
+            foreach ($cartItems as $item) {
                 OrderItem::create([
                     'order_id' => $order->id,
-                    'course_id' => $cartItem->course_id,
-                    'price' => $cartItem->price,
-                    'status' => 'active',
+                    'course_id' => $item->course_id,
+                    'price' => $item->course->price,
                 ]);
             }
 
-            // Xóa các mục trong giỏ hàng sau khi đã tạo đơn hàng
-            $cart->cartItems()->delete();
+            $cartItems->each->delete();
+            $cart->delete();
 
-            // Tạo PaymentIntent và lưu vào Order
-            $paymentIntent = $this->createPaymentIntentForOrder($order);
             DB::commit();
 
             return response()->json([
                 'status' => 'success',
-                'message' => 'Đơn hàng được tạo thành công',
-                'order' => $order,
-                'client_secret' => $paymentIntent['client_secret']
-            ], 201);
+                'message' => 'Order created successfully',
+            ]);
         } catch (\Exception $e) {
             DB::rollBack();
+            Log::error($e);
+
             return response()->json([
                 'status' => 'error',
-                'message' => 'Có lỗi xảy ra trong quá trình tạo đơn hàng',
-                'details' => $e->getMessage()
+                'message' => 'Order creation failed',
             ], 500);
         }
     }
 
-    private function createPaymentIntentForOrder($order)
+    public function success(Request $request)
     {
-        Stripe::setApiKey(config('services.stripe.secret'));
-
-        // Lấy thông tin người dùng hiện tại
-        $user = Auth::user();
-
-        // Tạo `Customer` nếu chưa tồn tại và lưu vào hồ sơ người dùng
-        if (!$user->stripe_customer_id) {
-            $customer = Customer::create([
-                'email' => $user->email,
-                'name' => $user->name,
-            ]);
-            $user->update(['stripe_customer_id' => $customer->id]);
-        } else {
-            $customer = Customer::retrieve($user->stripe_customer_id);
-        }
-
-        // Gắn `PaymentMethod` vào `Customer` nếu cần
-        $paymentMethod = 'pm_card_visa'; // Đây là ID mẫu của `PaymentMethod`, bạn có thể thay bằng ID từ request
-        $retrievedPaymentMethod = PaymentMethod::retrieve($paymentMethod);
-        if ($retrievedPaymentMethod->customer !== $customer->id) {
-            $retrievedPaymentMethod->attach(['customer' => $customer->id]);
-        }
-
-        // Tạo `PaymentIntent` với `Customer` và `PaymentMethod` đã gắn
-        $paymentIntent = PaymentIntent::create([
-            'amount' => $order->total_price * 100,
-            'currency' => $order->currency ?? 'usd',
-            // 'currency' => 'vnd',
-            'customer' => $customer->id,
-            'payment_method' => $paymentMethod,
-            'description' => 'Thanh toán đơn hàng #' . $order->order_code,
-            'setup_future_usage' => 'off_session', // Để lưu lại PaymentMethod cho các giao dịch trong tương lai
-            'automatic_payment_methods' => [
-                'enabled' => true,
-                'allow_redirects' => 'never'
-            ]
-        ]);
-
-        // Cập nhật `payment_code` với `PaymentIntent ID` từ Stripe
-        $order->update(['payment_code' => $paymentIntent->id]);
-
-        return ['client_secret' => $paymentIntent->client_secret];
-    }
-
-    public function listOrders(Request $request)
-    {
-        // Lấy user hiện tại
-        $userId = Auth::id();
-
-        // Lấy danh sách đơn hàng của người dùng, kèm thông tin OrderItems và khóa học
-        $orders = Order::where('user_id', $userId)
-            ->with(['orderItems.course'])
-            ->orderByDesc('created_at')
-            ->paginate(10); // Phân trang, mỗi trang 10 đơn hàng
-
-        // Trả về danh sách đơn hàng
-        return response()->json([
-            'status' => 'success',
-            'message' => 'Danh sách đơn hàng được lấy thành công',
-            'orders' => $orders
-        ], 200);
-    }
-
-    public function getOrderDetails($id)
-    {
-        // Xác định người dùng hiện tại
-        $userId = Auth::id();
-
-        // Tìm đơn hàng dựa trên ID và user_id để đảm bảo quyền truy cập
-        $order = Order::where('id', $id)
-            ->where('user_id', $userId)
-            ->with(['orderItems.course']) // Tải các mục trong đơn hàng và thông tin khóa học nếu có
-            ->first();
-
-        // Kiểm tra nếu đơn hàng không tồn tại hoặc không thuộc về người dùng hiện tại
-        if (!$order) {
+        if (!$request->session_id) {
             return response()->json([
                 'status' => 'error',
-                'message' => 'Đơn hàng không tồn tại hoặc bạn không có quyền truy cập'
-            ], 404);
-        }
-
-        // Trả về dữ liệu chi tiết của đơn hàng
-        return response()->json([
-            'status' => 'success',
-            'message' => 'Chi tiết đơn hàng được lấy thành công',
-            'order' => $order
-        ], 200);
-    }
-
-    public function confirmPayment(Request $request, $id)
-    {
-        Stripe::setApiKey(config('services.stripe.secret'));
-
-        // Lấy thông tin người dùng hiện tại
-        $user = Auth::user();
-
-        // Kiểm tra nếu người dùng đã có `Customer` trên Stripe
-        if (!$user->stripe_customer_id) {
-            $customer = Customer::create([
-                'email' => $user->email,
-                'name' => $user->name,
-            ]);
-            $user->update(['stripe_customer_id' => $customer->id]);
-        } else {
-            $customer = Customer::retrieve($user->stripe_customer_id);
-        }
-
-        // Lấy đơn hàng của người dùng hiện tại và kiểm tra trạng thái
-        $order = Order::where('id', $id)
-            ->where('user_id', $user->id)
-            ->where('payment_status', 'pending')
-            ->first();
-
-        if (!$order) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Đơn hàng không tồn tại hoặc đã được thanh toán'
-            ], 404);
-        }
-
-        try {
-            // Lấy PaymentIntent từ payment_code của đơn hàng
-            $paymentIntent = PaymentIntent::retrieve($order->payment_code);
-
-            // Xác nhận PaymentIntent với `allow_redirects: 'never'` để tắt các phương thức yêu cầu chuyển hướng
-            $paymentIntent->confirm([
-                'payment_method' => $request->payment_method_id
-            ]);
-
-            // Cập nhật trạng thái đơn hàng nếu thanh toán thành công
-            if ($paymentIntent->status == 'succeeded') {
-                $order->update([
-                    'payment_status' => 'paid',
-                    'status' => 'active',
-                ]);
-
-                return response()->json([
-                    'status' => 'success',
-                    'message' => 'Thanh toán thành công',
-                    'order' => $order
-                ], 200);
-            }
-
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Thanh toán thất bại hoặc chưa được xác nhận',
-                'payment_intent_status' => $paymentIntent->status
+                'message' => 'Session ID is required',
             ], 400);
-        } catch (\Exception $e) {
-            Log::error("Lỗi xác thực thanh toán: " . $e->getMessage());
-
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Lỗi xác thực thanh toán',
-                'error' => $e->getMessage()
-            ], 500);
         }
+        $stripe = new \Stripe\StripeClient(config('services.stripe.secret'));
+        $response = $stripe->checkout->sessions->retrieve($request->session_id);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Order completed successfully',
+            'data' => $response,
+        ], 201);
+    }
+
+    public function cancel(Request $request)
+    {
+        return response()->json([
+            'status' => 'error',
+            'message' => 'Order cancelled',
+        ], 400);
     }
 }
