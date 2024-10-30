c5c2ca81511830e699d42e64fadf3fb5594f2faf
Nguyen Quoc Hoc <Quochoc135@gmail.com>
2024-10-30 10:00:03 +0700

    push full ui admin

--- frontend/src/views/admin/Users/SettingsTeacher.vue
+++ frontend/src/views/admin/Users/SettingsTeacher.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import SubmitButtonPrimary from '@/components/admin/Button/SubmitButtonPrimary.vue';
+import RadioGroup from '@/components/admin/Dialog/RadioGroup.vue';
 import HeaderNavbar from '@/components/admin/Headernavbar/HeaderNavbar.vue';
 
 
@@ -10,7 +12,44 @@ import HeaderNavbar from '@/components/admin/Headernavbar/HeaderNavbar.vue';
       </HeaderNavbar>
     <div class="py-4">
         <div class="background-table">
+          <div class="py-4 px-2">
+            <span class="p-2 font-bold">Cài đặt phần trăm danh thu</span>
+            <div class="p-2">
+              <RadioGroup
+              label="Phần trăm danh thu của giáo viên"
+              >
+                <el-input
+                  style="max-width: 600px"
+                  class="cursor-move py-4"
+                >
+                  <template #title>Chương 1 nè</template>
+                  <template #append>
+                    %
+                  </template>
+                </el-input>
+              </RadioGroup>
+              <RadioGroup
+              label="Phần trăm danh thu của admin"
+              >
+                <el-input
+                  style="max-width: 600px"
+                  class="cursor-move py-4"
+                >
+                  <template #title>Chương 1 nè</template>
+                  <template #append>
+                    %
+                  </template>
+                </el-input>
+              </RadioGroup>
+              <div class="flex">
+                <SubmitButtonPrimary
+                title="Cập nhật"
+                />
+              </div>
+            </div>
+          </div>
         </div>
+
     </div>
 
     </div>
