af35cf947f9be1f8b8c5a5be39be4bd017719ac6
Đỗ Tuấn Kiệt <kietdtit@gmail.com>
2024-12-05 23:21:53 +0700

    fix course

--- backend/app/Http/Controllers/CourseController.php
+++ backend/app/Http/Controllers/CourseController.php
@@ -132,6 +132,7 @@ class CourseController extends Controller
     $thumbnailPath = $this->uploadThumbnail($request);
     $course = new Course();
     $course->fill($request->all());
+    $course->status='inactive';
     $course->thumbnail = $thumbnailPath;
     $course->created_by = auth()->id();
     $course->save();
