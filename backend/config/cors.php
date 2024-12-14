02809f97642bbb52d07d39452d2e00a1682ab031
Toan ne <toan70868@gmail.com>
2024-12-14 19:30:40 +0700

    refactor: add cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -15,7 +15,9 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'broadcasting/*', 'sanctum/csrf-cookie'],
+//    'paths' => ['api/*', 'broadcasting/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'broadcasting/*', 'sanctum/csrf-cookie', '/images/*', '/models/*'],
+
 
     'allowed_methods' => ['*'],
 
@@ -29,6 +31,11 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
+    'supportedContentTypes' => [
+        'model/gltf-binary',
+        'application/json',
+        'text/html',
+    ],
 
 ];
